1f4a8b8e7b3dc9f43cf12a7b9d0113faa5635ad8
Ibjyot Gill <igill47@my.bcit.ca>
2022-08-25 12:12:01 -0700

    adding comps

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './comps/Header';
+import Tasks from './comps/Tasks';
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <Tasks />
     </div>
 
   )
---
+++ src/comps/Tasks.js
@@ -0,0 +1,17 @@
+const tasks = [
+    {
+
+    }
+]
+
+export const Tasks = () => {
+  return (
+    <>
+        {tasks.map((task) => (
+            <h3>{task.text}</h3>
+        ))}
+    </>
+  )
+}
+
+export default Tasks
